70553d91b6ad975a6b9cb89cb45e3a50c4eb6290
Juan Jesús Silvestre Chávez <134343358+TYisusK@users.noreply.github.com>
2023-11-23 22:53:21 -0600

    Update compra.js

--- Models/compra.js
+++ Models/compra.js
@@ -1,8 +1,10 @@
 class Compra {
-  constructor(id, productos) {
+  constructor(id, productos, userId) {
     this.id = id; // ID de la compra
     this.productos = productos || []; // Array de productos comprados con sus cantidades
     this.fechaCompra = new Date(); // Fecha de la compra, por ejemplo
+    this.userId = userId;
+    this.bandera = 0; // Valor inicial de bandera
   }
 
   set id(id) {
@@ -16,12 +18,22 @@ class Compra {
     this._productos = productos;
   }
 
+  set userId(userId) {
+    if (userId != null) {
+      this._userId = userId;
+    }
+  }
+
   get id() {
     return this._id;
   }
 
   get productos() {
     return this._productos;
+  }
+
+  get userId() {
+    return this._userId;
   }
 
   // Función para agregar un producto a la compra con su cantidad
